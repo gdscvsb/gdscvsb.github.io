8af174ff15a4f088f8815204bc48ea13a8437a7f
gdscvsb <115916870+gdscvsb@users.noreply.github.com>
2022-10-16 19:04:40 +0530

    Update verify.js

--- compose/cf/verify.js
+++ compose/cf/verify.js
@@ -6,5 +6,66 @@ let data = [
     {
         "id": "9514180717",
         "name": "Nitthish Nagavel"
+    },
+    {
+        "id":"22GDSC9100",
+        "name":"KOWTHEM G V"
+    },
+    {
+        "id":"22GDSC9101",
+        "name":"DEHAVI D"
+    },
+    {
+        "id":"22GDSC9102",
+        "name":"ARUN L"
+    },
+    {
+        "id":"22GDSC9103",
+        "name":"TAMILSELVAN B"
+    },
+    {
+        "id":"22GDSC9104",
+        "name":"THILAK KAARTHIK P "
+    },
+    {
+        "id":"22GDSC9105",
+        "name":"HARIPRIYA C"
+    },
+    {
+        "id":"22GDSC9106",
+        "name":"VARSHINI.V "
+    },
+    {
+        "id":"22GDSC9107",
+        "name":"ISAIVANI.N"
+    },
+    {
+        "id":"22GDSC9108",
+        "name":"SOWMIYA K "
+    },
+    {
+        "id":"22GDSC9109",
+        "name":"VIDHYA P"
+    },
+    {
+        "id":"22GDSC9110",
+        "name":"SIVA PRABHA.S "
+    },
+    {
+        "id":"22GDSC9111",
+        "name":"SUDHARSAN"
+    },
+    {
+        "id":"22GDSC9112",
+        "name":"THARANI L"
+    },
+    {
+        "id":"22GDSC9113",
+        "name":"ANISH V"
+    },
+    {
+        "id":"22GDSC9200",
+        "name":"Vignesh Nagavel"
     }
+
 ]
